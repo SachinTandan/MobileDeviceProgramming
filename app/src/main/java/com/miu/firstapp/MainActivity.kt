b450ee0a399c6908965e1481eb78215e0136a6b9
schn916 <sachin8peace@gmail.com>
2022-10-23 18:16:04 -0500

    task A and B completed

---
+++ app/src/main/java/com/miu/firstapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.miu.firstapp
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.collections.random as random
+
+class MainActivity : AppCompatActivity() {
+    var foodList: ArrayList<String> =
+        arrayListOf<String>("Hamburger", "Pizza", "Mexican", "American", "Chinese")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //video note: interaction between the activity and layout
+        setContentView(R.layout.activity_main)
+
+        addFood.setOnClickListener {
+            foodList.add(editTextFoodName.text.toString())
+            showAlert()
+            editTextFoodName.setText("")
+        }
+    }
+
+    fun decide(view: View) {
+//        (editTextFoodName as TextView).text = foodList.random()
+        editTextFoodName.setText(foodList.random())
+    }
+
+    fun showAlert() {
+        val dialogBuilder = AlertDialog.Builder(this)
+        // set message of alert dialog
+        dialogBuilder.setMessage("New food added to list!").setCancelable(true)
+        // create dialog box
+        val alert = dialogBuilder.create()
+        // set title for alert dialog box
+        alert.setTitle("Food added!")
+        // show alert dialog
+        alert.show()
+    }
+
+}
